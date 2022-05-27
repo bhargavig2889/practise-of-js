abfdd23f44a1d36b774f7c170f56969f37fc285f
bhargavig2889 <105350113+bhargavig2889@users.noreply.github.com>
2022-05-27 13:59:58 +0530

    Add files via upload

---
+++ comparsion operators.js
@@ -0,0 +1,49 @@
+// = operator
+//const a=10;
+//const  b= 20;
+//const  c="heloo";
+
+//== operator
+//const a=5, b=2, c='Hello';
+ //console.log(a==5);
+ //console.log(b=='2');
+// console.log(c=='Hello');
+//===operator
+//const a=5, b=2, c='Hello';
+//console.log(a===5);
+// console.log(b==='2');
+ // console.log(c==='Hello');
+ //(!=)operators
+// const a=5, b=2, c='Hello';
+ // console.log(a!=3);
+  //console.log(b!=2);
+//(!==)operators
+//const a=2 ,b='Heloo';
+//console.log(a !== 2); 
+//console.log(a !== '2'); 
+ //console.log(b !== 'Hello');
+ //(>) opertaor
+ //const a=3;
+ //console.log(a>2);
+ //(>=)
+//const a=6;
+//console.log(a>=5);
+//(<) operator
+ //const a=3;
+ //console.log(a<4);
+//(<=)
+//const b=7;
+//console.log(b<=8);
+// logical operators
+//(&&) and operator
+//console.log(a && a); 
+// console.log(a && b);  
+
+// console.log((c > 2) && (c < 2)); 
+//(||)or  operator
+//console.log(a || b);
+//console.log(b || b); 
+// console.log((c>2) || (c<2));
+//(!) not operator
+//console.log(!a); 
+// console.log(!b); 
---
+++ conditional statements.js
@@ -0,0 +1,32 @@
+
+// if else if else statements
+const number=prompt("enter the number")
+if(number>0)
+{
+    console.log(" number is positive")
+}
+else if(number==0)
+    {
+    console.log(" number greater than r equal to 0")
+}
+else 
+{
+    console.log("number is negitive")
+}
+// nested if statement
+const number1=prompt("enter the number")
+
+if (number1 >= 0)
+ {
+  if (number1 == 0)
+   {
+           console.log("You entered number 0");
+        }
+         else 
+        {
+            console.log("You entered a positive number");
+        }
+    } 
+    else {
+        console.log("You entered a negative number");
+    }
